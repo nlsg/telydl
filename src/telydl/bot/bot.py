bc2d4594ef8b02299a96265394f9b4e20390e7e8
nils <kappeler.nils@gmail.com>
2026-01-30 00:43:45 +0100

    fix missing await

--- src/telydl/bot/bot.py
+++ src/telydl/bot/bot.py
@@ -306,7 +306,7 @@ class TelyDlBot:
         msg = f"bot error: {context.error}"
         _logger.error(msg)
         update.char_
-        context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
     async def _process_text_command(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
